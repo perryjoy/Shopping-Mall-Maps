6b4621ee5a4cffc1bd587c44cfe3baadc54b63e4
Fourroubles <60853520+Fourroubles@users.noreply.github.com>
2021-04-12 15:50:32 +0300

    Graph code refactor

--- code/core/graph.cpp
+++ code/core/graph.cpp
@@ -1,4 +1,7 @@
-#include "graph.h"
+#include "device/graph.h"
+#include <math.h>
+#include <algorithm>
+
 
 float graph::CalculateWeight(vec2 firstVertex, vec2 secondVertex)
 {
@@ -9,32 +12,31 @@ float graph::CalculateWeight(vec2 firstVertex, vec2 secondVertex)
 void graph::AddVertex(vertex_graph vertex)
 {
     adjacencyList.push_back(vertex);
-    for(auto vert : vertex.edgeWeight)
+    for(auto vert : vertex.weightedEdges)
     {
-        adjacencyList[vert.vertex_ID].edgeWeight.push_back({(long int)adjacencyList.size() - 1, vert.weight});
+        adjacencyList[vert.vertex_ID].weightedEdges.push_back({(long int)adjacencyList.size() - 1, vert.weight});
     }
 }
 
 void graph::DeleteVertex(int index)
 {
-    for(auto & vert : adjacencyList)
+    for(auto vert : adjacencyList)
     {
-
-        auto edgeForDeleting = std::find_if(vert.edgeWeight.begin(), vert.edgeWeight.end(), [&](edge adj)
+        auto edgeForDeleting = std::find_if(vert.weightedEdges.begin(), vert.weightedEdges.end(), [&](edge adj)
         {
             return adj.vertex_ID == index;
         });
 
-        if (edgeForDeleting != vert.edgeWeight.end())
+        if (edgeForDeleting != vert.weightedEdges.end())
         {
-            vert.edgeWeight.erase(edgeForDeleting);
+            vert.weightedEdges.erase(edgeForDeleting);
         }
     }
 
     adjacencyList.erase(adjacencyList.begin() + index);
 }
 
-vec2 graph::MethodKramer(std::vector<float> firstEquation, std::vector<float> secondEquation)
+vec2 graph::MethodKramer(const std::vector<float> & firstEquation, const std::vector<float> & secondEquation)
 {
     vec2 newCoordinate;
     newCoordinate.x = (firstEquation[2] * secondEquation[1] - firstEquation[1] * secondEquation[2]) /
@@ -48,8 +50,8 @@ vec2 graph::MethodKramer(std::vector<float> firstEquation, std::vector<float> se
 std::vector<float> graph::CreateLineEquation(vertex_graph vert, int index)
 {
     std::vector<float> lineEquation;
-    lineEquation.push_back(- vert.vertexCoordinates.y + adjacencyList[index].vertexCoordinates.y);
-    lineEquation.push_back(- adjacencyList[index].vertexCoordinates.x + vert.vertexCoordinates.x);
+    lineEquation.push_back(-vert.vertexCoordinates.y + adjacencyList[index].vertexCoordinates.y);
+    lineEquation.push_back(-adjacencyList[index].vertexCoordinates.x + vert.vertexCoordinates.x);
     lineEquation.push_back(vert.vertexCoordinates.x * adjacencyList[index].vertexCoordinates.y -
                            adjacencyList[index].vertexCoordinates.x * vert.vertexCoordinates.y);
 
@@ -61,19 +63,19 @@ std::vector<float> graph::CreateNormalEquation(vertex_graph vert, int index, vec
     std::vector<float> normalEquation;
     normalEquation.push_back(adjacencyList[index].vertexCoordinates.x - vert.vertexCoordinates.x);
     normalEquation.push_back(vert.vertexCoordinates.y - adjacencyList[index].vertexCoordinates.y);
-    normalEquation.push_back(- vert.vertexCoordinates.x * coordinates.x + adjacencyList[index].vertexCoordinates.x * coordinates.x +
+    normalEquation.push_back(-vert.vertexCoordinates.x * coordinates.x + adjacencyList[index].vertexCoordinates.x * coordinates.x +
                              coordinates.y * vert.vertexCoordinates.y - adjacencyList[index].vertexCoordinates.y * coordinates.y);
 
     return normalEquation;
 }
 
-std::vector<edge> graph::CopyDataEdgeWeight(vec2 newVertex, edge adjVert, int h)
+std::vector<edge> graph::CopyDataweightedEdges(vec2 newVertex, edge adjVert, int h)
 {
-    std::vector<edge> edgeWeight;
-    edgeWeight.push_back({h,CalculateWeight(adjacencyList[h].vertexCoordinates, newVertex)});
-    edgeWeight.push_back({adjVert.vertex_ID,CalculateWeight(adjacencyList[adjVert.vertex_ID].vertexCoordinates,newVertex)});
+    std::vector<edge> weightedEdges;
+    weightedEdges.push_back({h,CalculateWeight(adjacencyList[h].vertexCoordinates, newVertex)});
+    weightedEdges.push_back({adjVert.vertex_ID,CalculateWeight(adjacencyList[adjVert.vertex_ID].vertexCoordinates,newVertex)});
 
-    return edgeWeight;
+    return weightedEdges;
 }
 
 void graph::AddTemporaryVertex(vec2 coordinates)
@@ -82,11 +84,11 @@ void graph::AddTemporaryVertex(vec2 coordinates)
     vertex_graph newVertex;
     float weight = 0;
     int index = 0;
-    float minimalWeight = CalculateWeight(MethodKramer(CreateLineEquation(adjacencyList[0], adjacencyList[0].edgeWeight[0].vertex_ID),
-                          CreateNormalEquation(adjacencyList[0], adjacencyList[0].edgeWeight[0].vertex_ID, coordinates)), coordinates);
+    float minimalWeight = CalculateWeight(MethodKramer(CreateLineEquation(adjacencyList[0], adjacencyList[0].weightedEdges[0].vertex_ID),
+                          CreateNormalEquation(adjacencyList[0], adjacencyList[0].weightedEdges[0].vertex_ID, coordinates)), coordinates);
     for(auto & vert : adjacencyList)
     {
-        for(auto & adjVert : vert.edgeWeight)
+        for(auto & adjVert : vert.weightedEdges)
         {
             if(index < adjVert.vertex_ID &&
                std::max(vert.vertexCoordinates.x, adjacencyList[adjVert.vertex_ID].vertexCoordinates.x) >= coordinates.x &&
@@ -100,7 +102,7 @@ void graph::AddTemporaryVertex(vec2 coordinates)
                 {
                     minimalWeight = weight;
                     newVertex.vertexCoordinates = newCoordinate;
-                    newVertex.edgeWeight = CopyDataEdgeWeight(newCoordinate, adjVert, index);
+                    newVertex.weightedEdges = CopyDataweightedEdges(newCoordinate, adjVert, index);
                     newVertex.vertexFloor = vert.vertexFloor;
                 }
             }
@@ -108,10 +110,8 @@ void graph::AddTemporaryVertex(vec2 coordinates)
 
         index++;
     }
-
-    std::cout<<newVertex.vertexCoordinates.x<<" "<<newVertex.vertexCoordinates.y<<std::endl;
     AddVertex(newVertex);
-    AddVertex({{coordinates}, newVertex.vertexFloor, {{(long int)adjacencyList.size() - 1,CalculateWeight(coordinates, newVertex.vertexCoordinates)}}});
+    AddVertex({{coordinates}, newVertex.vertexFloor, {{static_cast<long int>(adjacencyList.size()) - 1,CalculateWeight(coordinates, newVertex.vertexCoordinates)}}});
 }
 
 std::vector<int> graph::SearchWay(int vertexStart, int vertexFinish)
@@ -119,10 +119,10 @@ std::vector<int> graph::SearchWay(int vertexStart, int vertexFinish)
     const int INF = INT_MAX;
     std::vector<int> path;
     size_t graphSize = adjacencyList.size();
-    std::vector<int> minimalWay (graphSize, INF);
-    std::vector<int> temporatyValue (graphSize);
+    std::vector<int> minimalWay(graphSize, INF);
+    std::vector<int> temporatyValue(graphSize);
     minimalWay[vertexStart] = 0;
-    std::vector<bool> isVisited (graphSize);
+    std::vector<bool> isVisited(graphSize);
     for (size_t i = 0; i < graphSize; ++i)
     {
         int unattainableValue = -1;
@@ -139,13 +139,13 @@ std::vector<int> graph::SearchWay(int vertexStart, int vertexFinish)
 
         isVisited[unattainableValue] = true;
 
-        for (size_t j = 0; j < adjacencyList[unattainableValue].edgeWeight.size(); ++j)
+        for (size_t j = 0; j < adjacencyList[unattainableValue].weightedEdges.size(); ++j)
         {
-            int to = adjacencyList[unattainableValue].edgeWeight[j].vertex_ID,
-            len = adjacencyList[unattainableValue].edgeWeight[j].weight;
-            if (minimalWay[unattainableValue] + len < minimalWay[to])
+            int to = adjacencyList[unattainableValue].weightedEdges[j].vertex_ID,
+            weight = adjacencyList[unattainableValue].weightedEdges[j].weight;
+            if (minimalWay[unattainableValue] + weight < minimalWay[to])
             {
-                minimalWay[to] = minimalWay[unattainableValue] + len;
+                minimalWay[to] = minimalWay[unattainableValue] + weight;
                 temporatyValue[to] = unattainableValue;
             }
         }
